9d6f643349ab0d2f899cf53aa0c48bef61cd0b4a
zys-chd <1725472881@qq.com>
2022-01-14 16:09:58 +0800

    project structure change

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
+"""
+name: TkinkerFileExplorer
+version: 0.0.3
+author: zys-chd
+author_email: yusong_zhang@outlook.com
+description: A simple and efficient file explorer written in python tkinker
+"""
+
+import os
+import sys
+import tkinter as tk
+import tkinter.ttk as ttk
+import ttkthemes
+
+
+print(sys.platform)
+
+
+
+
 name = "TkinkerFileExplorer"
--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+# -*- coding: utf-8 -*-# -*- coding: utf-8 -*-
 import setuptools
+import os
+import requests
+import io
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+if os.path.exists("requirements.txt"):
+    install_requires = io.open("requirements.txt").read().split("\n")
+else:
+    install_requires = []
+
 setuptools.setup(
     name="TkinkerFileExplorer",
-    version="0.0.1",
+    version="0.0.3",
     author="zys-chd",
     author_email="yusong_zhang@outlook.com",
     description="A simple and efficient file explorer written in python tkinker",
@@ -15,7 +25,18 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Development Status :: 1 - Planning",
         "Operating System :: OS Independent",
     ],
+    include_package_data=True,
+    install_requires=install_requires,
+        package_data = {
+            # If any package contains *.txt or *.rst files, include them:
+            # 'TkinkerFileExplorer': ['source/*.txt', "source/*.json"],
+    },
+    entry_points={
+        'console_scripts': [
+            # 'TkinkerFileExplorer = TkinkerFileExplorer.run:main'
+        ]
+    }
 )
